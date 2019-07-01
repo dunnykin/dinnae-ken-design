945b93fe6686ce6a1e0a4ab6fcec24819110aca0
dunnykin <optimusjim@hotmail.co.uk>
2019-07-02 00:02:11 +0100

    Adding about and contact pages, links to footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,8 @@
-
-    </body>
+<div>
+    <ul>
+        <li><a href="/dinnae-ken-design/php/about.php">About</a></li>
+        <li><a href="/dinnae-ken-design/php/contact.php">Contact</a></li>
+    </ul>
+</div>
+</body>
 </html>
---
+++ php/about.php
@@ -0,0 +1,9 @@
+<?php
+$title = "About";
+include("../includes/header.php");   
+?>
+<h1><?php echo $title; ?></h1>
+
+<?php
+include("../includes/footer.php");   
+?>

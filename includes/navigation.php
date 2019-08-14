a052e53ef8e962d1d5ea6a013c40d3bb25c610a9
dunnykin <optimusjim@hotmail.co.uk>
2019-08-15 00:21:44 +0100

    Added design logos

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,6 +1,11 @@
 <div class="nav-bar">
     <ul>
-        <li><a href="/dinnae-ken-design/php/index.php">Home</a></li>
+        <li>
+            <a class="logo" href="/dinnae-ken-design/php/index.php">
+                <?php echo file_get_contents("../assets/images/dinnaeken-design-logo.svg"); ?>
+                <?php echo file_get_contents("../assets/images/dinnaeken-design-txt.svg"); ?>
+            </a>
+        </li>
         <li><a href="/dinnae-ken-design/php/web-design.php">Web Design</a></li>
         <li><a href="/dinnae-ken-design/php/game-dev.php">Game Development</a></li>
         <li><a href="/dinnae-ken-design/php/3d-animation.php">3D Animation</a></li>

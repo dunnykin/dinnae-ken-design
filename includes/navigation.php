22cfcd4c94fb2a947e5e87f92d2cccc8c0e4e1cc
dunnykin <optimusjim@hotmail.co.uk>
2019-08-17 21:40:18 +0100

    Eric Meyer Reset. More header grid.

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,14 +1,18 @@
 <header>
     <div class="nav-bar">
-        <a class="logo" href="/dinnae-ken-design/php/index.php">
-            <span class="logo-icon"><?php echo file_get_contents("../assets/images/dinnaeken-design-logo.svg"); ?></span>
-            <span class="logo-text"><?php echo file_get_contents("../assets/images/dinnaeken-design-txt.svg"); ?></span>
+        <a class="logo-icon" href="/dinnae-ken-design/php/index.php">
+            <?php echo file_get_contents("../assets/images/dinnaeken-design-logo.svg"); ?>
         </a>
-        <ul class="nav-list">
-            <li><a href="/dinnae-ken-design/php/web-design.php">Web Design</a></li>
-            <li><a href="/dinnae-ken-design/php/game-dev.php">Game Development</a></li>
-            <li><a href="/dinnae-ken-design/php/3d-animation.php">3D Animation</a></li>
-            <li><a href="/dinnae-ken-design/php/graphic-design.php">Graphic Design</a></li>
-        </ul>
+        <a class="logo-text" href="/dinnae-ken-design/php/index.php">
+            <?php echo file_get_contents("../assets/images/dinnaeken-design-txt.svg"); ?>
+        </a>
+        <nav class="nav-list">
+            <ul>
+                <li><a href="/dinnae-ken-design/php/web-design.php">Web Design</a></li>
+                <li><a href="/dinnae-ken-design/php/game-dev.php">Game Development</a></li>
+                <li><a href="/dinnae-ken-design/php/3d-animation.php">3D Animation</a></li>
+                <li><a href="/dinnae-ken-design/php/graphic-design.php">Graphic Design</a></li>
+            </ul>
+        </nav>
     </div>
 </header>

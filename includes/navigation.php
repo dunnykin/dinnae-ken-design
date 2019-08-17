da78c2f669b83b4235c991129d95075ecae85ea1
dunnykin <optimusjim@hotmail.co.uk>
2019-08-17 13:08:07 +0100

    Added glow to logos. Logo grid.

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,14 +1,14 @@
-<div class="nav-bar">
-    <ul>
-        <li>
-            <a class="logo" href="/dinnae-ken-design/php/index.php">
-                <?php echo file_get_contents("../assets/images/dinnaeken-design-logo.svg"); ?>
-                <?php echo file_get_contents("../assets/images/dinnaeken-design-txt.svg"); ?>
-            </a>
-        </li>
-        <li><a href="/dinnae-ken-design/php/web-design.php">Web Design</a></li>
-        <li><a href="/dinnae-ken-design/php/game-dev.php">Game Development</a></li>
-        <li><a href="/dinnae-ken-design/php/3d-animation.php">3D Animation</a></li>
-        <li><a href="/dinnae-ken-design/php/graphic-design.php">Graphic Design</a></li>
-    </ul>
-</div>
+<header>
+    <div class="nav-bar">
+        <a class="logo" href="/dinnae-ken-design/php/index.php">
+            <span class="logo-icon"><?php echo file_get_contents("../assets/images/dinnaeken-design-logo.svg"); ?></span>
+            <span class="logo-text"><?php echo file_get_contents("../assets/images/dinnaeken-design-txt.svg"); ?></span>
+        </a>
+        <ul class="nav-list">
+            <li><a href="/dinnae-ken-design/php/web-design.php">Web Design</a></li>
+            <li><a href="/dinnae-ken-design/php/game-dev.php">Game Development</a></li>
+            <li><a href="/dinnae-ken-design/php/3d-animation.php">3D Animation</a></li>
+            <li><a href="/dinnae-ken-design/php/graphic-design.php">Graphic Design</a></li>
+        </ul>
+    </div>
+</header>

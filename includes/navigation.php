87e1f35b2e4c30f19682f4b640532348126a3539
dunnykin <optimusjim@hotmail.co.uk>
2019-11-18 01:06:44 +0000

    WIP - redesign

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,18 +1,28 @@
 <header>
-    <div class="nav-bar">
-        <a class="logo-icon" href="/dinnae-ken-design/php/index.php">
-            <?php echo file_get_contents("../assets/images/dinnaeken-design-logo.svg"); ?>
+    <!-- <div class="nav-bar"> -->
+        <!-- <a class="logo-icon" href="/dinnae-ken-design/php/index.php">
+            <//?php echo file_get_contents("../assets/images/dinnaeken-design-logo.svg"); ?>
         </a>
         <a class="logo-text" href="/dinnae-ken-design/php/index.php">
-            <?php echo file_get_contents("../assets/images/dinnaeken-design-txt.svg"); ?>
-        </a>
-        <nav class="nav-list">
+            <//?php echo file_get_contents("../assets/images/dinnaeken-design-txt.svg"); ?>
+        </a> -->
+        <!-- <nav class="nav-list">
             <ul>
                 <li><a href="/dinnae-ken-design/php/web-design.php">Web Design</a></li>
                 <li><a href="/dinnae-ken-design/php/game-dev.php">Game Development</a></li>
                 <li><a href="/dinnae-ken-design/php/3d-animation.php">3D Animation</a></li>
-                <li><a href="/dinnae-ken-design/php/graphic-design.php">Graphic Design</a></li>
             </ul>
         </nav>
-    </div>
+    </div> -->
+    <secion class="nav-wrapper">
+        <a class="nav-icon" href="/dinnae-ken-design/php/index.php">
+            <?php echo file_get_contents("../assets/images/dinnaeken-design-logo.svg"); ?>
+        </a>
+        <a class="nav-logo" href="/dinnae-ken-design/php/index.php">
+            <?php echo file_get_contents("../assets/images/dinnaeken-design-txt.svg"); ?>
+        </a>
+        <div class="nav-link design">design</div>
+        <div class="nav-link game-dev">game dev</div>
+        <div class="nav-link three-d">3D</div>
+    </secion>
 </header>
--- php/index.php
+++ php/index.php
@@ -4,33 +4,52 @@ include("../includes/header.php");
 ?>
 
 <div id="home-wrapper">
-    <div class="hero">
+    
+    <section class="hero">
         <h2>What does good design mean to you?</h2>
         <h1>Dinnae Ken</h1>
 
-    </div>
+    </section>
+
     
-    <div class="services">
-    <ul>
-        <li>
+    <section class="services">
+        <ul class="services-list">
+            <li class="services-item design">
+                <i class="fas fa-laptop"></i>
+                <h3>Web Design</h3>
+            </li>
+            <li class="services-item">
+                <i class="fas fa-gamepad"></i>
+                <h3>Game Development</h3>
+            </li>
+            <li class="services-item">
+                <i class="fas fa-cube"></i>
+                <h3>3D Animation</h3>
+            </li>
+        </ul>
+        <!-- <div class="services-item design">
             <i class="fas fa-laptop"></i>
             <h3>Web Design</h3>
-        </li>
-        <li>
+        </div>
+        <div class="services-item">
             <i class="fas fa-gamepad"></i>
             <h3>Game Development</h3>
-        </li>
-        <li>
+        </div>
+        <div class="services-item">
             <i class="fas fa-cube"></i>
             <h3>3D Animation</h3>
-        </li>
-        <li>
-            <i class="fas fa-pencil-ruler"></i>
-            <h3>Graphic Design</h3>
-        </li>
-    </ul>
+        </div> -->
+    </section>
 
-    </div>
+    <section class="gallery">
+        <div class="gallery-item medium">one</div>
+        <div class="gallery-item large two">two</div>
+        <div class="gallery-item medium">three</div>
+        <div class="gallery-item small">four</div>
+        <div class="gallery-item tall">five</div>
+        <div class="gallery-item wide">six</div>
+    </section>
+
 </div>
 
 <?php

d7ea7dd8753fa3a4cb424251325bab27bc64e17f
dunnykin <optimusjim@hotmail.co.uk>
2019-11-24 21:04:01 +0000

    Modal images

--- php/design.php
+++ php/design.php
@@ -14,7 +14,7 @@ include("../includes/header.php");
         <p>However, each one should demonstrate a design technique or two:</p>
     </div>
 
-    <div class="product-container design-darkness">
+    <div class="product-container design-darkness" onclick="openModal();currentSlide(1)">
         <img src="../assets/images/the-darkness.jpg" />
         <div class="product-overlay">
             <div class="product product-header">
@@ -33,7 +33,7 @@ include("../includes/header.php");
         </div>
     </div>
 
-    <div class="product-container design-nordic-tall">
+    <div class="product-container design-nordic-tall" onclick="openModal();currentSlide(3)">
         <img src="../assets/images/nordic-spring-02.jpg" />
         <div class="product-overlay">
             <div class="product product-header">
@@ -52,7 +52,7 @@ include("../includes/header.php");
         </div>
     </div>
 
-    <div class="product-container design-swatch">
+    <div class="product-container design-swatch" onclick="openModal();currentSlide(2)">
         <img src="../assets/images/swatch-poster.png" />
         <div class="product-overlay">
             <div class="product product-header">
@@ -71,7 +71,7 @@ include("../includes/header.php");
         </div>
     </div>
 
-    <div class="product-container design-nordic-wide">
+    <div class="product-container design-nordic-wide" onclick="openModal();currentSlide(4)">
         <img src="../assets/images/nordic-spring-01.jpg" />
         <div class="product-overlay">
             <div class="product product-header">
@@ -90,7 +90,7 @@ include("../includes/header.php");
         </div>
     </div>
 
-    <div class="product-container design-smoke">
+    <div class="product-container design-smoke" onclick="openModal();currentSlide(5)">
         <img src="../assets/images/anti-smoking-trump.jpg" />
         <div class="product-overlay">
             <div class="product product-header">
@@ -109,7 +109,7 @@ include("../includes/header.php");
         </div>
     </div>
 
-    <div class="product-container design-death">
+    <div class="product-container design-death" onclick="openModal();currentSlide(6)">
         <img src="../assets/images/Animation2.0.gif" />
         <div class="product-overlay">
             <div class="product product-header">
@@ -128,7 +128,7 @@ include("../includes/header.php");
         </div>
     </div>
 
-    <div class="product-container design-halifax">
+    <div class="product-container design-halifax" onclick="openModal();currentSlide(7)">
         <img src="../assets/images/halifax.png" />
         <div class="product-overlay">
             <div class="product product-header">
@@ -148,6 +148,80 @@ include("../includes/header.php");
     </div>
 
 </section>
+
+<div id="myModal" class="modal">
+  <span class="close cursor" onclick="closeModal()">&times;</span>
+  <div class="modal-content">
+
+    <div class="modal-slide">
+      <div class="numbertext">1 / 7</div>
+      <img src="../assets/images/the-darkness.jpg">
+    </div>
+
+    <div class="modal-slide">
+      <div class="numbertext">2 / 7</div>
+      <img src="../assets/images/nordic-spring-02.jpg">
+    </div>
+
+    <div class="modal-slide">
+      <div class="numbertext">3 / 7</div>
+      <img src="../assets/images/nordic-spring-01.jpg">
+    </div>
+    
+    <div class="modal-slide">
+      <div class="numbertext">4 / 7</div>
+      <img src="../assets/images/swatch-poster.png">
+    </div>
+
+    <div class="modal-slide">
+      <div class="numbertext">5 / 7</div>
+      <img src="../assets/images/anti-smoking-trump.jpg">
+    </div>
+
+    <div class="modal-slide">
+      <div class="numbertext">6 / 7</div>
+      <img src="../assets/images/Animation2.0.gif">
+    </div>
+
+    <div class="modal-slide">
+      <div class="numbertext">7 / 7</div>
+      <img src="../assets/images/halifax.png">
+    </div>
+    
+    <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+    <a class="next" onclick="plusSlides(1)">&#10095;</a>
+
+    <div class="caption-container">
+      <p id="caption"></p>
+    </div>
+
+    <div class="columns">
+        <div class="column">
+        <img class="slide cursor" src="../assets/images/the-darkness.jpg" style="width:100%" onclick="currentSlide(1)" alt="The Darkness mock album art">
+        </div>
+        <div class="column">
+        <img class="slide cursor" src="../assets/images/nordic-spring-02.jpg" style="width:100%" onclick="currentSlide(2)" alt="Fictional water brand, Nordic Spring">
+        </div>
+        <div class="column">
+        <img class="slide cursor" src="../assets/images/nordic-spring-01.jpg" style="width:100%" onclick="currentSlide(3)" alt="Fictional water brand, Nordic Spring">
+        </div>
+        <div class="column">
+        <img class="slide cursor" src="../assets/images/swatch-poster.png" style="width:100%" onclick="currentSlide(4)" alt="Swatch mock poster">
+        </div>
+        <div class="column">
+        <img class="slide cursor" src="../assets/images/anti-smoking-trump.jpg" style="width:100%" onclick="currentSlide(5)" alt="Anti-smoking campaign poster">
+        </div>
+        <div class="column">
+        <img class="slide cursor" src="../assets/images/Animation2.0.gif" style="width:100%" onclick="currentSlide(6)" alt="Death & the Dwarf, 2D animation">
+        </div>
+        <div class="column">
+        <img class="slide cursor" src="../assets/images/halifax.png" style="width:100%" onclick="currentSlide(7)" alt="Social marketing art for Twitch streamer Halifax">
+        </div>
+    </div>
+  </div>
+</div>
+
+<script src="../assets/js/modal-images.js"></script>
 
 <?php
 include("../includes/footer.php");   

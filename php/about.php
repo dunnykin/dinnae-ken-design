69980a6fa09f827f2c1c8f3f37e563920bf5e5b2
dunnykin <optimusjim@hotmail.co.uk>
2019-11-26 22:29:13 +0000

    About page complete.

--- php/about.php
+++ php/about.php
@@ -8,8 +8,27 @@ $image = "https://www.dinnaeken.co.uk/assets/images/dinnaekengames-header.webp";
 $imageAlt = "Dinnae Ken Design - Design, 3D & Game Development";
 include("../includes/header.php");   
 ?>
-<h1><?php echo $title; ?></h1>
+<h1 class="page-title">Who am I?</h1>
+<section class="about-wrapper">
+    <p class="about-intro">My name is JAMIE MORRISON and I am the creator of DINNAE KEN GAMES, DINNAE KEN DESIGN and DINNAE KEN 3D. These areas cover the breadth of my work, the best of which combines all three.</p>
 
+    <p class="about-chunk"><span class="about-heading">DINNAE KEN GAMES</span>
+    <br>Game development, predominantly for PC. Whilst my main focus is on Unity 3D and C#, I also have experience developing other types of games using Adobe Animate, Game  Maker Studio and more.</p>
+
+    <p class="about-chunk"><span class="about-heading">DINNAE KEN DESIGN</span>
+    <br>In the course of earning my degree, I have developed skills in many areas such as vector and bitmap graphic design, website and app design, video production and 2D animation.</p>
+
+    <p class="about-chunk"><span class="about-heading">DINNAE KEN 3D</span>
+    <br>I have been developing my 3D skillset through the creation of models and animations, mainly using 3DS Max, however I have begun to make the switch to Blender, and am now facing the task of transferring skills into a different piece of software and managing a new UI.</p>
+
+    <p class="about-me"><span class="about-heading">A LITTLE BIT ABOUT ME</span>
+    <br>I am an avid PC gamer, so when I’m not developing games I am likely playing them. I also have qualificaitons in acting & performance and have even tried my hand at stand-up comedy, giving me the confidence to perform in front of the camera as well as behind it.</p>
+
+    <figure class="profile-pic">
+        <img src="../assets/images/profile-pic.webp" alt="Profile picture of Jamie Morrison, creator behind the Dinnae Ken interactive media brand."/>
+        <figcaption>Who's this? Dinnae Ken</figcaption>
+    </figure>
+</section>
 <?php
 include("../includes/footer.php");   
 ?>

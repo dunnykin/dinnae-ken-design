ee5f8034cea7eb4688e394b10578be31a6cf8fa6
dunnykin <optimusjim@hotmail.co.uk>
2019-11-26 23:18:36 +0000

    Remove max-height from svg and add favicon to head

--- includes/header.php
+++ includes/header.php
@@ -38,6 +38,7 @@ if (preg_match('~MSIE|Internet Explorer~i', $ua) || (strpos($ua, 'Trident/7.0') 
     <link rel="stylesheet" type="text/css" media="screen" href="../assets/css/style.css" />
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+    <link rel="icon" href="../favicon.ico" type="image/x-icon" />
     
 </head>
 <body> <!-- Container closes in footer -->

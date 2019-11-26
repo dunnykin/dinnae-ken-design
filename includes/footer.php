95f858bb9f34939a8a69003ddfb4ee342a4d79b8
dunnykin <optimusjim@hotmail.co.uk>
2019-11-26 18:11:44 +0000

    Footer styling

--- includes/footer.php
+++ includes/footer.php
@@ -1,9 +1,26 @@
 </main>
-    <footer>
-        <ul>
-            <li><a href="/php/about.php">About</a></li>
-            <li><a href="/php/contact.php">Contact</a></li>
-        </ul>
+    <footer class="footer">
+        <div class="footer-wrapper">
+            <!-- <div class="footer-copy">
+                <p>Copyright Dinnae Ken Games</p>
+            </div> -->
+            <ul class="footer-nav">
+                <li><a href="/php/about.php">About</a></li>
+                <li><a href="/php/contact.php">Contact</a></li>
+            </ul>
+            <ul class="social-nav">
+                <li>
+                    <a href="https://www.facebook.com/dinnaekengames/">
+                        <i class="fab fa-twitter"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://twitter.com/dinnaekengames">
+                        <i class="fab fa-facebook"></i>
+                    </a>
+                </li>
+            </ul>
+        </div>
     </footer>
 </body>
 </html>

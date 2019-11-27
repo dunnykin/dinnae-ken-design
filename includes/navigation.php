5be642f7e6af5c5964519aa46db3695cfa59571a
dunnykin <optimusjim@hotmail.co.uk>
2019-11-27 03:28:24 +0000

    Added conditional statement to logos

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,10 +1,20 @@
 <header>
     <section class="nav-wrapper">
         <a class="nav-icon" href="/php/index.php">
-            <?php echo file_get_contents("../assets/images/svg/dinnaeken-design-logo.svg"); ?>
+            <?php if($dkName == "Dinnae Ken Design")
+                echo file_get_contents("../assets/images/svg/dinnaeken-design-logo.svg");
+            else
+                echo file_get_contents("../assets/images/svg/dinnaeken-games-logo.svg");
+            ?>
         </a>
         <a class="nav-logo" href="/php/index.php">
-            <?php echo file_get_contents("../assets/images/svg/dinnaeken-design-txt.svg"); ?>
+            <?php echo file_get_contents("../assets/images/svg/dinnaeken-design-txt.svg");
+            // Show "design" or "games"
+            if ($dkName == "Dinnae Ken Design")
+                echo '<style>.logo-txt-games{display:none}</style>';
+            else
+                echo '<style>.logo-txt-design{display:none}</style>';
+            ?>
         </a>
         <nav class="nav-bar">
             <ul class="nav-links">
@@ -12,6 +22,6 @@
                 <li class="nav-link"><a href="/php/game-dev.php">Game Development</a></li>
                 <li class="nav-link"><a href="/php/3d-animation.php">3D Animation</a></li>
             </ul>
-        </nav
+        </nav>
     </section>
 </header>
